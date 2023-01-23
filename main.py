165d714a49694cd45e97000bbb7e82734cb4de22
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2023-01-24 01:40:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from DB_func import mlh
 app = FastAPI()
+origins = [
+    "http://localhost",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/static",StaticFiles(directory='static'),name="static")
 templates = Jinja2Templates(directory="templates")
 # @app.get("/users/{user_id}",response_class=HTMLResponse)
@@ -14,4 +26,7 @@ def welcome(request:Request):
 @app.get("/hackathons",response_class=HTMLResponse)
 def hackathons(request:Request):
     return templates.TemplateResponse("hackathons.html",{"request":request})
+@app.get("/hackathons/mlh")
+def mlh_hac_all():
+    return mlh.fetchAll()
     
