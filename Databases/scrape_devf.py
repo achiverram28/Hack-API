a08a179ea7c7a182199fe96beff333361ae55022
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2023-01-24 10:37:36 +0530

    Update scrape_devf.py

--- Databases/scrape_devf.py
+++ Databases/scrape_devf.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd 
 event_names=list()
 event_dates=list()
+event_months=list()
 link_lists=["https://hackshetra-23.devfolio.co/","https://octahacks.devfolio.co/","https://bitnbuild.netlify.app/","https://ingenious-hackathon-4.devfolio.co/","https://lnmhacks.devfolio.co/","https://castor-2023.devfolio.co/","https://unscript2k23.devfolio.co/","https://ethforall.devfolio.co/","https://hackoverflow2.devfolio.co/","https://dotslash-6.devfolio.co/","https://sbssu-smart-hackathon.devfolio.co/","https://hack-infinity.devfolio.co/","https://unscript-rookies-hackathon-k.devfolio.co/","https://technex-hackathon-2.devfolio.co/","https://hack-jmi.devfolio.co/","https://vashist.devfolio.co/","https://reckon-4.devfolio.co/","https://hackbrewer.devfolio.co/","https://diversion2k23.devfolio.co/","https://duhacks2.devfolio.co/","https://hackmol-2.devfolio.co/","https://ur-hackathon-2.devfolio.co/","https://innerve-seven.devfolio.co/","https://nebula.devfolio.co/","https://cybershadez-23.devfolio.co/","https://hult-prize-bvcoe-1.devfolio.co/","https://hackbeats.devfolio.co/","https://nawabathon.devfolio.co/","https://edu-hack.devfolio.co/","https://edu-hack.devfolio.co/","https://nmithacks.devfolio.co/"]
 for li in link_lists:
   topic_url = li
@@ -27,7 +28,8 @@ for li in link_lists:
        if tags_date[i].text in event_dates:
          continue
        event_dates.append(tags_date[i].text)
+  event_months=[ob.split(" ")[0] for ob in event_dates]
 # print(event_dates)
 ################################################
-dataframe_set = pd.DataFrame(list(zip(event_names,event_dates)),columns=["Names","Date and Time"])
-dataframe_set.to_csv("Hac4.csv")
+dataframe_set = pd.DataFrame(list(zip(event_names,event_dates,event_months)),columns=["Names","Date and Time","Month"])
+dataframe_set.to_csv("Hac2.csv")
