63d3d852029f80fff8fc7ef6fb7520b6bd3f2666
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2023-01-29 16:55:22 +0530

    Delete app directory

--- app/database.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-import certifi
-import urllib
-client=pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote_plus("ram")+":"+urllib.parse.quote_plus("123456789!@#$%^&*")+"@api-dev.gwndkto.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
-db=client["fastapi"]
-User = db["users"]
