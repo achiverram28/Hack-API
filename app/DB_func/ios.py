621f59d99102ad25bb9055470807e3441438d40b
achiverram28 <ramsamarth21bcs24@iiitkottayam.ac.in>
2023-02-08 20:21:02 +0530

    Deta Release versions

---
+++ app/DB_func/ios.py
@@ -0,0 +1,30 @@
+import pymongo
+import urllib
+import certifi
+client=pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote_plus("ram")+":"+urllib.parse.quote_plus("123456789!@#$%^&*")+"@api-dev.gwndkto.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+db=client["Hackathons"]
+events = db["iOS-Events"]
+def fetchAll():
+    x = events.find({},{'_id':False,"Unnamed: 0":False})
+    li=[]
+    for ob in x:
+        if ob in li:
+            continue
+        li.append(ob)
+    return li
+def fetch_desc(name):
+    s = name.split("_")
+    st=""
+    for it in s:
+        st=st+it+" "
+    st=st.strip()
+    x = events.find({"Name":st},{"_id":False,"Unnamed: 0":False})
+    li=[]
+    for i in x:
+        if i not in li:
+          if i["Name"]==st:
+             li.append(i)
+    return li[0]
+def fetch_name_desc(name):
+    res=fetch_desc(name)
+    return [res["Description"]]
